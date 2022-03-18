73fadb0ae2e2330b359029b19612a841e363872a
HppyKazuki <c1141120@planet.kanazawa-it.aac.jp>
2022-03-18 11:38:31 +0900

    add python exercise

---
+++ src/python_edu_1_2_3.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+#-*- coding: utf-8 -*-
+
+#######################################
+#title: python講習会第1,2回演習問題3問目
+#author: Kazuki Shiba
+#date: 2022/3/18
+#######################################
+
+integer = int(input('2桁以上の整数を入力してください: '))
+a = integer ** 2
+b = integer // 10
+print(a, b)
